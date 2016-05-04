57cd3ec29fad7e8a056d0c753124a54e89fafbc6
Nobuki Okauchi <n008d1@gmail.com>
2016-05-04 17:53:21 +0900

    jekins実行バッチ追加

---
+++ jenkins/reload.bat
@@ -0,0 +1,23 @@
+@rem カレントディレクトリはtestClock直下とする
+@echo off
+
+rem ========================================================
+rem プロセスが起動してたら終了
+rem 起動してなかったらエラー出るけど気にしない
+set PROCESS_NAME=electron.exe
+Taskkill /IM "%PROCESS_NAME%" /F
+rem --------------------------------------------------------
+
+
+rem ========================================================
+rem アプリ起動
+set SRC_DIR=src
+set ELECTRON=electron
+
+echo start
+call %ELECTRON% "%SRC_DIR%"
+rem --------------------------------------------------------
+
+echo finish
+exit
+
